abe34e60b576a4ff958d03995de56c7c003a63e0
CooperKozitza <cooperkozitza@gmail.com>
2022-10-08 22:03:37 -0700

    Created Tileset and Updated Tile Prefab

--- Assets/LevelGen/TilePrototype.cs
+++ Assets/LevelGen/TilePrototype.cs
@@ -7,23 +7,33 @@ using System;
 
 public class TilePrototype : MonoBehaviour
 {
+    public List<TilePrototype> Entropy = new List<TilePrototype>();
+    public bool Collapsed = false;
 
     [Serializable]
-    public class Side
+    public class Face
     {
         public int connectorId;
         [Range(0, 3)]
         public int rotation = 0;
         public bool flipped = false;
+        public bool invariable = false;
+        public Dictionary<TilePrototype, int> ProbabilityOverrides = new Dictionary<TilePrototype, int>();
+    }
+
+    [Serializable]
+    public class VerticalFace : Face
+    {
+        public bool symetrical;
     }
 
     public bool walkable = false;
 
-    public Side Top;
-    public Side Bottom;
-    public Side Left;
-    public Side Right;
-    public Side Front;
-    public Side Back;
+    public VerticalFace Top;
+    public VerticalFace Bottom;
+    public Face Left;
+    public Face Right;
+    public Face Front;
+    public Face Back;
 
 }
