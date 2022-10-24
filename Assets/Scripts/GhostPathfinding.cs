560771f0ecba2b9a8cbf5329d05defe8effc90c5
peterplatosh <peter@platosh.com>
2022-10-24 13:47:32 -0700

    Incremental
    8c2b0264bcaef74145dd0dee428a645b7ccbe179

--- Assets/Scripts/GhostPathfinding.cs
+++ Assets/Scripts/GhostPathfinding.cs
@@ -3,31 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GhostPathfinding : MonoBehaviour
-{
-    [Range(0, 5)]
+{ 
     public float speed;
 
-    private Rigidbody rigidBody { get; set; }
+    private Rigidbody RigidBody { get; set; }
 
     public Vector3 targetPosition;
     public Vector3 distanceVector;
 
+    private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        RigidBody = GetComponent<Rigidbody>();
         targetPosition = new Vector3(0, 0, 0);
-        
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            Debug.Log("Player not found");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        distanceVector = transform.position - targetPosition;
-        rigidBody.AddForce(new Vector3(distanceVector.x, 0, distanceVector.y));
-        if (rigidBody.velocity.magnitude > 10)
+        targetPosition = player.transform.position;
+        distanceVector = targetPosition - transform.position;
+        RigidBody.AddForce(new Vector3(distanceVector.x, 0, distanceVector.z).normalized * speed);
+        if (RigidBody.velocity.magnitude > speed)
         {
-            rigidBody.velocity = rigidBody.velocity.normalized * 10;
+            RigidBody.velocity = RigidBody.velocity.normalized * speed;
         }
         //rigidBody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rigidBody.velocity.y, Input.GetAxis("Vertical") * speed);
         
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [Range(0, 5)]
     public float speed;
+    public float maxSpeed;
     public float floatiness = 1.0f;
 
     private Rigidbody RigidBody { get; set; }
@@ -20,19 +20,33 @@ public class GhostMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RigidBody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, RigidBody.velocity.y, Input.GetAxis("Vertical") * speed);
+        RigidBody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * speed);
+
         if (Input.GetButton("Jump") && onGround)
         {
             RigidBody.AddForce(new Vector3(0, floatiness, 0));
-            onGround = false;
         }
+
+        if (RigidBody.velocity.magnitude > maxSpeed)
+        {
+            RigidBody.velocity = RigidBody.velocity.normalized * maxSpeed;
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Floor"))
         {
-            onGround = true;
+           onGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            onGround = false;
         }
     }
 }
