e0662025a4430f0cf0e22a6deaa234e8e2aa0447
peterplatosh <peter@platosh.com>
2022-11-03 15:10:22 -0700

    Fixed Pathfinding

--- Assets/Scripts/NavMeshMovement.cs
+++ Assets/Scripts/NavMeshMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine.AI;
 
 public class NavMeshMovement : MonoBehaviour
 {
-    public Transform goal;
+    public Vector3 goal;
 
     private GameObject player;
 
     NavMeshAgent agent;
+
+    public bool seePlayer;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +24,41 @@ public class NavMeshMovement : MonoBehaviour
             Debug.Log("Player not found");
         }
 
-        goal = player.transform;
+        goal = transform.position;
+        agent.destination = goal;
 
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        agent.destination = goal.position;
+    {
+        Vector3 rayDirection = player.transform.position - transform.position;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, rayDirection.normalized, out hit, rayDirection.magnitude))
+        {
+            if (hit.transform.gameObject.CompareTag("Player"))
+            {
+                goal = hit.transform.position;
+                Debug.Log("see player");
+                seePlayer = true;
+                Debug.DrawRay(transform.position, rayDirection.normalized * hit.distance, Color.yellow);
+            }
+            else
+            {
+                if (seePlayer) {
+                    goal = hit.point;
+                    seePlayer = false;
+                }
+                Debug.DrawRay(transform.position, rayDirection.normalized * hit.distance, Color.magenta);
+                Debug.DrawRay(transform.position, goal - transform.position, Color.blue);
+            }
+        }
+
+        if (Vector3.Distance(goal, transform.position) > 0.1f)
+        {
+            agent.destination = goal;
+        }
     }
 }
